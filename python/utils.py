4196b90dabe6b6957d48e728905796e37c9b60c3
Maria Benito <mariabenitocst@gmail.com>
2020-11-27 17:20:12 +0200

    Fixed typo

--- python/utils.py
+++ python/utils.py
@@ -69,7 +69,7 @@ def temperature_withDM(r, heat_int, f=1, R=R_jup.value, M=M_jup.value,
     """
     Exoplanet temperature : internal heating + DM heating
     """
-    return (np.power(((heat_int + heat_DM(r, f=f, R=R, M=M, 
+    return (np.power((heat_int + heat_DM(r, f=f, R=R, M=M, 
                      parameters=parameters))/
                      (4*np.pi*R**2*sigma_sb.value*epsilon), 1./4.))
 
