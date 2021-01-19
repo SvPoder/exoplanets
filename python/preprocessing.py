3c4033d689a510135bfe0e8f6833a1fb94f19da2
maria benito <mariabenito@MacBook-Air-de-maria.local>
2021-01-19 18:57:37 +0100

    nBDs

--- python/preprocessing.py
+++ python/preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle
 
-def statistics(filepath, rel_unc_Tobs, rank=100, D=2):
+def statistics(filepath, nBDs, rank=100, D=2):
     """
     Calculate mean, median, MAP & ML point estimates
 
@@ -19,11 +19,10 @@ def statistics(filepath, rel_unc_Tobs, rank=100, D=2):
     
     for i in range(rank):
         # load posterior + likelihood
-        file_name = (filepath + "posterior_game0_uncTobs_" + str(rel_unc_Tobs) 
-                   + "v" + str(i))
+        file_name = (filepath + ("posterior_game0_nBDs_%iv%i" %(nBDs, i)))
         samples   = pickle.load(open(file_name, "rb"))
-        file_name = (filepath + "likelihood_game0_uncTobs_" + str(rel_unc_Tobs) 
-                   + "v" + str(i))
+
+        file_name = (filepath + ("likelihood_game0_nBDs%iv%i" %(nBDs, i)))
         like      = pickle.load(open(file_name, "rb"))
         # calculate point estimates
         for j in range(D):
@@ -34,7 +33,7 @@ def statistics(filepath, rel_unc_Tobs, rank=100, D=2):
             MAP[j][i]    = _bins[np.argmax(_n)]
             ML[j][i]     = samples[:, j][np.argmax(like)]
 
-    output = open(("../results/bayesian/statistics_game0_uncTobs_%.2f.dat" %rel_unc_Tobs), 
+    output = open(("../results/bayesian/statistics_game0_nBDs_%i.dat" %nBDs), 
                   "w")
     for i in range(rank):
         for j in range(D):
@@ -54,7 +53,8 @@ def statistics(filepath, rel_unc_Tobs, rank=100, D=2):
 
 if __name__ == '__main__':
     filepath     = "../results/bayesian/"
-    rel_unc_Tobs = [0.01, 0.02, 0.03, 0.05, 0.1, 0.25, 0.5]
-    for rel_unc in rel_unc_Tobs:
-        statistics(filepath, rel_unc)
+    #rel_unc_Tobs = [0.01, 0.02, 0.03, 0.05, 0.1, 0.25, 0.5]
+    nBDs = [1000, 5000, 20000, 50000]
+    for n in nBDs:
+        statistics(filepath, n)
 
