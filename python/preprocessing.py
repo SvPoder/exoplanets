10e2a3e12b912c8a90396ec411091083c80591dd
mariabenitocst <mariabenitocst@gmail.com>
2021-03-12 17:53:01 +0200

    update

--- python/preprocessing.py
+++ python/preprocessing.py
@@ -15,19 +15,14 @@ def statistics(filepath, nBDs, rel_unc, relM, f, gamma, rs, rank=100, D=2):
     mean   = np.zeros((D, rank))
     median = np.zeros((D, rank))
     MAP    = np.zeros((D, rank))
-    ML     = np.zeros((D, rank))
     
     for i in range(rank):
         #print(i)
         # load posterior + likelihood
 
-        file_name = (filepath + ("posterior_ex3_N%i_relunc%.2f_relM%.2f_f%.1fgamma%.1frs%.1fv%i" 
-                    %(nBDs, rel_unc, relM, f, gamma, rs, i)))
+        file_name = (filepath + ("N%irelT%.2frelM%.2f/posterior_ex3_N%i_relunc%.2f_relM%.2f_f%.1fgamma%.1frs%.1fv%i" 
+                    %(nBDs, rel_unc, relM, nBDs, rel_unc, relM, f, gamma, rs, i+1)))
         samples   = pickle.load(open(file_name, "rb"))
-
-        file_name = (filepath + ("likelihood_ex3_N%i_relunc%.2f_relM%.2f_f%.1fgamma%.1frs%.1fv%i" 
-                    %(nBDs, rel_unc, relM, f, gamma, rs, i)))
-        like      = pickle.load(open(file_name, "rb"))
 
         # calculate point estimates
         for j in range(D):
@@ -36,8 +31,7 @@ def statistics(filepath, nBDs, rel_unc, relM, f, gamma, rs, rank=100, D=2):
             #TODO need to change # bins to see if results differ
             _n, _bins    = np.histogram(samples[:, j], bins=50)
             MAP[j][i]    = _bins[np.argmax(_n)]
-            ML[j][i]     = samples[:, j][np.argmax(like)]
-
+    filepath = "/home/mariacst/exoplanets/results/"
     output = open(filepath + ("statistics_ex3_N%i_relunc%.2f_relM%.2f_f%.1fgamma%.1frs%.1f" 
                               %(nBDs, rel_unc, relM, f, gamma, rs)), 
                   "w")
@@ -48,8 +42,6 @@ def statistics(filepath, nBDs, rel_unc, relM, f, gamma, rs, rank=100, D=2):
             output.write("%.4f  " %median[j][i])
         for j in range(D):
             output.write("%.4f  " %MAP[j][i])
-        for j in range(D):
-            output.write("%.4f  " %ML[j][i])
         output.write("\n")
     output.close()
 
@@ -58,13 +50,13 @@ def statistics(filepath, nBDs, rel_unc, relM, f, gamma, rs, rank=100, D=2):
 
 
 if __name__ == '__main__':
-    filepath = "/Users/mariabenito/Desktop/results/"
-    nBDs     = [10000]
+    filepath = "/scratch/mariacst/exoplanets/results/"
+    nBDs     = [1000]
     rel_unc  = [0.1]
-    rel_M    = [0.1]
+    rel_M    = [0.1, 0.2]
     f        = 1.
     rs       = [5., 10., 20.]
-    gamma    = [0., 0.5, 1., 1.3, 1.5]
+    gamma    = [1.1, 1.2, 1.4]
 
     for N in nBDs:
         for rel in rel_unc:
