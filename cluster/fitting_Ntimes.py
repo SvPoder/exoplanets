5371fba8893e27b8083c63705ec0b2da62a93902
mariabenitocst <mariabenitocst@gmail.com>
2021-04-29 15:26:30 +0300

    cluster

---
+++ cluster/fitting_Ntimes.py
@@ -0,0 +1,92 @@
+import sys
+sys.path.append("/home/mariacst/cluster/.env/lib/python3.6/site-packages")
+import emcee
+import numpy as np
+from scipy.interpolate import griddata
+from utils import heat, temperature_withDM
+from mock_generation import mock_population_all
+from astropy.constants import R_jup, M_sun
+import glob
+import pickle
+import time
+
+start = time.time()
+
+# Local DM density
+rho0 = 0.42 # GeV/cm3
+
+ex         = sys.argv[1]
+rank       = int(sys.argv[2])
+nBDs       = int(sys.argv[3])
+relTobs    = float(sys.argv[4])
+relM       = float(sys.argv[5])
+relRobs    = float(sys.argv[6])
+relA       = float(sys.argv[7])
+f_true     = 1.
+gamma_true = float(sys.argv[8])
+rs_true    = float(sys.argv[9])
+try:
+    v = float(sys.argv[8])
+except:
+    v = None
+#print(v)
+# --------- MCMC fitting -> change to another file -----------------------
+def lnprior(p):
+    f, gamma, rs = p
+    if ( 0. < gamma < 3. and 0. < f < 1. and 0.01 < rs < 50.):
+        return 0.
+    return -np.inf
+
+def residual(p):#, robs, Tobs, rel_unc_Tobs, heat_int, mass):
+    f, gamma, rs = p
+    Tmodel = temperature_withDM(robs, heat_int, f=f, M=mass*M_sun.value, 
+                                parameters=[gamma, rs, rho0], v=v)
+    return -0.5*np.sum(((Tmodel-Tobs)/(relTobs*Tobs))**2.)
+
+
+def lnprob(p):#, robs, Tobs, rel_unc_Tobs, heat_int, mass):
+    lp = lnprior(p)
+    if not np.isfinite(lp):
+        # Return
+        return -np.inf
+    # Return
+    return lp + residual(p)#, robs, Tobs, rel_unc_Tobs, heat_int, mass)
+# ------------------------------------------------------------------------
+
+# Load theoretical cooling model
+path = "./data/"
+data = np.genfromtxt(path + "./ATMO_CEQ_vega_MIRI.txt", unpack=True)
+points = np.transpose(data[0:2, :])
+values = data[2]
+# Generate mock observation
+robs, Tobs, mass, ages = mock_population_all(nBDs, 
+                                         relTobs, relM, relRobs, relA,
+                                         f_true, gamma_true, rs_true, 
+                                         rho0_true=rho0, 
+                                         v=v)
+## calculate predictic intrinsic heat flow for mock BDs
+xi       = np.transpose(np.asarray([ages, mass]))
+Teff     = griddata(points, values, xi)
+heat_int = heat(Teff, np.ones(len(Teff))*R_jup.value)
+# ------------------ RECONSTRUCTION --------------------------------------
+ndim     = 3
+nwalkers = 50
+# first guess
+p0 = [[0.9, 0.9, 20.] + 1e-4*np.random.randn(ndim) for j in range(nwalkers)]
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob) 
+                                #args=(r_obs, Tobs, rel_unc_T, heat_int, mass))
+pos, prob, state  = sampler.run_mcmc(p0, 300, progress=False)
+sampler.reset()
+pos, prob, state  = sampler.run_mcmc(pos, 5000, progress=False)
+# Save posterior
+filepath    = ("./results/N%irelT%.2frelM%.2f/posterior_" 
+               %(nBDs, relTobs, relM) + ex)
+file_object = open(filepath + 
+                   ("_N%i_relunc%.2f_relM%.2f_f%.1fgamma%.1frs%.1f" 
+                    %(nBDs, relTobs, relM, f_true, gamma_true, rs_true))
+                    + "v" + str(rank), "wb")
+pickle.dump(sampler.flatchain, file_object, protocol=2)
+file_object.close()
+
+
+print("Running time in s =", time.time()-start)
---
+++ cluster/utils.py
@@ -0,0 +1,86 @@
+import numpy as np
+from astropy.constants import R_jup, M_jup, G, sigma_sb
+from scipy.special import hyp2f1
+from scipy.interpolate import interp1d
+import astropy.units as u
+
+
+def vc(Rsun, Rint, parameters):
+    data = np.genfromtxt("./data/rc_e2bulge_R08.178_J_corr.dat", unpack=True)
+    r = data[0]
+    vB = data[1]
+    data = np.genfromtxt("./data/rc_hgdisc_R08.178_corr.dat", unpack=True)
+    vD = data[1]
+    vDM = vgNFW(Rsun, r, parameters)
+    vtot = np.sqrt(np.power(vB, 2) + np.power(vD, 2)+ np.power(vDM, 2))
+    vtot_intp = interp1d(r, vtot)
+    return vtot_intp(Rint)
+
+def vgNFW(Rsun, R, parameters):
+    """
+    Rotation velocity for gNFW dark matter density profile
+    """
+    # gNFW parameters
+    gamma = parameters[0]
+    Rs    = parameters[1]
+    rho0  = parameters[2] 
+    v     = []; 
+    for Rint in R:
+        hyp=np.float(hyp2f1(3-gamma,3-gamma,4-gamma,-Rint/Rs))
+        Integral=(-2**(2+3*gamma)*np.pi*Rint**(3-gamma)*(1+
+                  Rsun*(1./Rs))**(3-gamma)*rho0*hyp)/(-3+gamma)
+        v.append(np.sqrt(1.18997*10.**(-31.)*Integral/Rint)*3.08567758*10.**(16.))
+    v = np.array(v,dtype=np.float64)      
+    # Return
+    return v
+
+def gNFW_rho(Rsun, R, parameters):
+    """
+    Return gNFW density profile at r distance from the GC
+    Denstiy has same units as local DM density rho0
+    """
+    # gNFW parameters
+    gamma = parameters[0] 
+    Rs    = parameters[1]
+    rho0  = parameters[2]
+    # Density profile
+    rho   = rho0*np.power(Rsun/R, gamma)*np.power((Rs+Rsun)/(Rs+R), 3-gamma)    
+    # Return
+    return rho
+
+def heat_DM(r, f=1, R=R_jup.value, M=M_jup.value, Rsun=8.178, 
+            parameters=[1., 20., 0.42], v=None):
+    """
+    Heat flow due to DM capture and annihilation
+    """
+    vesc   = (np.sqrt(2*G*M/R)).value*1e-3 # m/s
+    if v:
+        _vD = v
+        #print(_vD, "inside")
+    else: 
+        _vD = np.sqrt(3/2.)*vc(Rsun, r, parameters) # km/s
+        #print(_vD, "RC value")
+    _vDM   =  np.sqrt(8./(3*np.pi))*_vD # km/s
+    _rhoDM = gNFW_rho(Rsun, r, parameters) # GeV/cm3
+
+    conversion_into_w = 0.16021766 
+
+    # return
+    return (f*np.pi*R**2*_rhoDM*_vDM*(1+3./2.*np.power(vesc/_vD, 2))*
+            conversion_into_w) # W
+
+def temperature_withDM(r, heat_int, f=1, R=R_jup.value, M=M_jup.value, 
+		parameters=[1., 20., 0.42], v=None, epsilon=1):
+    """
+    Exoplanet temperature : internal heating + DM heating
+    """
+    return (np.power((heat_int + heat_DM(r, f=f, R=R, M=M, 
+                     parameters=parameters, v=v))/
+                     (4*np.pi*R**2*sigma_sb.value*epsilon), 1./4.))
+
+def temperature(heat, R, epsilon=1):
+    return np.power(heat/(4*np.pi*R**2*sigma_sb*epsilon), 0.25)
+
+def heat(temp, R, epsilon=1):
+        return (4*np.pi*R**2*sigma_sb.value*temp**4*epsilon)
+
