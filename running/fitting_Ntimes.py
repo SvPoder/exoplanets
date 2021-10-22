cc8cbebc81386206f7cd6cfa82c3fe219b9e5987
Sven Põder <sven@manivald.local>
2021-10-22 11:29:36 +0300

    Sven's optimisation branch

--- running/fitting_Ntimes.py
+++ running/fitting_Ntimes.py
@@ -8,18 +8,22 @@ import imp
 import mock_generation
 imp.reload(mock_generation)
 from mock_generation import mock_population_all
-from astropy.constants import R_jup
+from astropy.constants import R_jup, M_jup, G, sigma_sb
 import glob
 import pickle
 #import time
-from emcee_functions import lnprob
+#from emcee_functions import lnprob
 
-#start = time.time()
+# Timing the script
+import time
+start = time.time()
 
 # Constant parameters & conversions ==========================================
 rho0                    = 0.42 # Local DM density [GeV/cm3]
 epsilon                 = 1.
 Rsun                    = 8.178 # Sun galactocentric distance [kpc]
+_sigma_sb = sigma_sb.value
+_G        = G.value
 # ============================================================================
 # Input parameters
 ex         = sys.argv[1]
@@ -37,54 +41,250 @@ path = "/home/mariacst/exoplanets/running/data/"
 data = np.genfromtxt(path + "./ATMO_CEQ_vega_MIRI.txt", unpack=True)
 points = np.transpose(data[0:2, :])
 values = data[2]
+
 # Mock observation
-#np.random.seed(rank)
+np.random.seed(36)
 (robs, sigmarobs, Tobs, sigmaTobs, Mobs,
      sigmaMobs, Aobs, sigmaAobs) = mock_population_all(nBDs, relTobs, sigma,
                                       sigma, sigma, f_true, gamma_true,
                                       rs_true, rho0_true=rho0, v=v)
-# Calculate derivatives Tint wrt Age and Mass                               
-dervTint_A = np.ones(nBDs)                                                  
-dervTint_M = np.ones(nBDs)                                                  
-size       = 7000                                                           
-h          = 0.001                                                          
-for i in range(nBDs):                                                       
-    dervTint_A[i] = derivativeTint_wrt_A(Mobs[i], Aobs[i], points, values,  
-                                         size=size, h=h)                        
-    dervTint_M[i] = derivativeTint_wrt_M(Mobs[i], Aobs[i], points, values,  
-                                         size=size, h=h)  
+
+# Calculate derivatives Tint wrt Age and Mass
+dervTint_A = np.ones(nBDs)
+dervTint_M = np.ones(nBDs)
+size       = 7000
+h          = 0.001
+
+# Path for 10^5 derivatives
+path_der = "/hdfs/local/mariacst/exoplanets/data_der/"
+
+steps = 200
+# for i in range(steps):
+
+#     # Load derivatives Tint wrt Age and Mass
+#     data = np.genfromtxt(path_der + "derivativeTint_" + ex + "_N%i_sigma%.1f_v%i.dat"
+#                     %(nBDs, sigma, i+1), unpack=True)
+
+#     dervTint_A = data[0]
+#     dervTint_M = data[1]
+
+
+for i in range(nBDs):
+    dervTint_A[i] = derivativeTint_wrt_A(Mobs[i], Aobs[i], points, values,
+                                         size=size, h=h)
+    dervTint_M[i] = derivativeTint_wrt_M(Mobs[i], Aobs[i], points, values,
+                                         size=size, h=h)
 
 ## calculate predictic intrinsic temperature
 xi       = np.transpose(np.asarray([Aobs, Mobs]))
 Teff     = griddata(points, values, xi)
+
+elapsed = time.time() - start
+print("Input section took " + str(elapsed))
+
+# ---------------------- emcee funcs ----------------------------------------
+
+from derivatives import derivativeTDM_wrt_M_emcee, derivativeTDM_wrt_r_emcee
+from utils import T_DM, temperature_withDM, gNFW_rho, vc
+
+
+def T_DM_optimised(r, R=R_jup.value, M=M_jup.value, Rsun=8.178, f=1.,
+         params=[1., 20., 0.42], v=None, epsilon=1., gNFW_rho = 0):
+    """
+    DM temperature
+    """
+    # escape velocity
+    vesc   = np.sqrt(2*_G*M/R)*1e-3 # km/s
+    if v:
+        _vD = v
+    else:
+        _vD    = np.sqrt(3/2.)*vc(Rsun, r, params) # km/s
+
+    _vDM   =  np.sqrt(8./(3*np.pi))*_vD # km/s
+    _rhoDM = gNFW_rho # GeV/cm3
+    # return
+    return np.power((f*_rhoDM*_vDM*(1+3./2.*np.power(vesc/_vD, 2))*
+                    conversion_into_w)/(4*_sigma_sb*epsilon), 1./4.)
+
+def temperature_withDM_optimised(r, Tint, R=R_jup.value, M=M_jup.value,
+                       f=1., p=[1., 20., 0.42], v=None, Rsun=8.178, epsilon=1, TDM =0):
+    """
+    Exoplanet temperature : internal heating + DM heating
+    """
+    return (np.power(np.power(Tint, 4) +
+                     np.power(TDM, 4)
+                     , 0.25))
+
+
+
+# Constant parameters & conversions ==========================================
+conversion_into_K_vs_kg = 1.60217e-7
+conversion_into_w       = 0.16021766
+conv_Msun_to_kg         = 1.98841e+30 # [kg/Msun]
+rho0                    = 0.42 # Local DM density [GeV/cm3]
+epsilon                 = 1.
+Rsun                    = 8.178 # Sun galactocentric distance [kpc]
+# ============================================================================
+
+def lnprior(p):
+    f, gamma, rs = p
+    if ( 0. < gamma < 3. and 0.01 < f < 2. and 0.01 < rs < 70.):
+        return 0.
+    return -np.inf
+
+def sigma_Tmodel2(r, M, A, sigma_r, sigma_M, sigma_A,
+                  Tint, points, values, dervTint_M, dervTint_A, f, params,
+                  v=None, R=R_jup.value, Rsun=8.178, epsilon=1, TDM = 0, gNFW_rho = 0):
+    """
+    Return squared uncertainty in model temperature [UNITS??]
+
+    Input:
+        r : Galactocentric distance [kpc]
+        M : mass [Msun]
+        A : age [Gyr]
+
+    Assumption: uncertainties in age, mass and galactocentric distance
+        are independent
+    """
+    #M_in_kg = M*conv_Msun_to_kg
+
+    _TDM = TDM
+    Ttot = np.power(_TDM**4 + Tint**4, 0.25)
+
+    ###########################################################################
+    # DELETE THIS IF DOESNT WORK <-- Shaves about 0,5 sec on each iteration
+
+    TintDivTtot = (Tint/Ttot)**3
+    TDMDivTtot = (_TDM/Ttot)**3
+
+    dervT_M = (TintDivTtot*dervTint_M +
+               TDMDivTtot*derivativeTDM_wrt_M_emcee(r, f, params, M, v=v, R=R,
+                                                  Rsun=Rsun,epsilon=epsilon, gNFW_rho=gNFW_rho))
+    # return
+    return (np.power(TintDivTtot*dervTint_A*sigma_A, 2)+
+            np.power(dervT_M*sigma_M, 2)+
+            np.power(TDMDivTtot*derivativeTDM_wrt_r_emcee(r, f, params, M, v=v,
+                                  R=R, Rsun=Rsun, epsilon=epsilon, gNFW_rho=gNFW_rho)*sigma_r, 2))
+
+
+    ###########################################################################
+
+    # dervT_M = ((Tint/Ttot)**3*dervTint_M +
+    #            (_TDM/Ttot)**3*derivativeTDM_wrt_M_emcee(r, f, params, M, v=v, R=R,
+    #                                               Rsun=Rsun,epsilon=epsilon, gNFW_rho=gNFW_rho))
+    # # return
+    # return (np.power((Tint/Ttot)**3*dervTint_A*sigma_A, 2)+
+    #         np.power(dervT_M*sigma_M, 2)+
+    #         np.power((_TDM/Ttot)**3*derivativeTDM_wrt_r_emcee(r, f, params, M, v=v,
+    #                               R=R, Rsun=Rsun, epsilon=epsilon, gNFW_rho=gNFW_rho)*sigma_r, 2))
+
+def residual(p):
+    """
+    Log likelihood function (without normalization!)
+    """
+    # unroll free parameters
+    f, gamma, rs = p
+
+    _gNFW_rho = gNFW_rho(Rsun, R_jup.value, [gamma, rs, rho0])
+
+    _TDM = T_DM_optimised(robs, R=R_jup.value, M=Mobs*conv_Msun_to_kg, Rsun=Rsun, f=f,
+         params=[gamma, rs, rho0], v=v, epsilon=epsilon, gNFW_rho = _gNFW_rho)
+
+    # model temperature [K]
+    Tmodel = temperature_withDM_optimised(robs, Teff, M=Mobs*conv_Msun_to_kg, f=f,
+                                p=[gamma, rs, rho0], v=v, TDM = _TDM)
+
+
+    _sigma_Tmodel2 = sigma_Tmodel2(robs, Mobs, Aobs, sigmarobs, sigmaMobs,
+                                   sigmaAobs, Teff, points, values, dervTint_M,
+                                   dervTint_A,
+                                   f, [gamma, rs, rho0], v=v, R=R_jup.value, Rsun=Rsun,
+                                   epsilon=epsilon, TDM = _TDM, gNFW_rho = _gNFW_rho)
+    # return
+    return -0.5*np.sum((Tmodel-Tobs)**2/(sigmaTobs**2 + _sigma_Tmodel2))
+
+
+def lnprob(p):
+    lp = lnprior(p)
+
+    if not np.isfinite(lp):
+        # Return
+        return -np.inf
+
+    # Return
+    return lp + residual(p)
+
+# import cProfile
+# def run_residual_profile():
+
+#     ndim     = 3
+#     nwalkers = 150
+#     p0 = [[0.9, 0.9, 20.] + 1e-4*np.random.randn(ndim) for j in range(nwalkers)]
+#     sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
+
+#     pos, prob, state  = sampler.run_mcmc(p0, 10, progress=True)
+
+#     print(pos)
+
+
+# cProfile.run('run_residual_profile()')
+
+
 # ------------------ RECONSTRUCTION --------------------------------------
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+ncpu = 6
+
 ndim     = 3
 nwalkers = 150
 print(nwalkers)
+
 # first guess
 p0 = [[0.9, 0.9, 20.] + 1e-4*np.random.randn(ndim) for j in range(nwalkers)]
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob,
-              args=(robs, sigmarobs, Mobs, sigmaMobs, Aobs, sigmaAobs, Tobs,
-                    sigmaTobs, Teff, points, values, dervTint_M, dervTint_A,
-                    v, R_jup.value, Rsun, rho0, epsilon))
 
-pos, prob, state  = sampler.run_mcmc(p0, 200, progress=False)
-sampler.reset()
-pos, prob, state  = sampler.run_mcmc(pos, 6000, progress=False)
+with Pool(ncpu) as pool:
+
+    # sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool = pool,
+    #             args=(robs, sigmarobs, Mobs, sigmaMobs, Aobs, sigmaAobs, Tobs,
+    #                     sigmaTobs, Teff, points, values, dervTint_M, dervTint_A,
+    #                     v, R_jup.value, Rsun, rho0, epsilon))
+
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool = pool)
+
+    print("Starting emcee")
+    start = time.time()
+
+    steps = 6000
+    pos, prob, state  = sampler.run_mcmc(p0, 200, progress=False)
+
+    sampler.reset()
+    pos, prob, state  = sampler.run_mcmc(pos, steps, progress=True)
+
+    elapsed = time.time() - start
+    print("Emcee took " + str(elapsed))
+
+
 
 # Save likelihood
 _path = "/hdfs/local/mariacst/exoplanets/results/likelihood/velocity/v100/fixedT10/"
-filepath    = (_path + "N%isigma%.1fb/like_" %(nBDs, sigma) + ex)
+_path = "/home/sven/repos/exoplanets/"
+#filepath    = (_path + "N%isigma%.1fb/like_" %(nBDs, sigma) + ex)
+filepath = (_path + "/like_" + ex)
 file_object = open(filepath + ("_N%i_sigma%.1f_f%.1fgamma%.1frs%.1f"
-                    %(nBDs, sigma, f_true, gamma_true, rs_true))            
-                    + "v" + str(rank), "wb") 
+                    %(nBDs, sigma, f_true, gamma_true, rs_true))
+                    + "v" + str(rank), "wb")
 pickle.dump(sampler.flatlnprobability, file_object, protocol=2)
-file_object.close() 
+file_object.close()
+
 # Save posterior
 _path = "/hdfs/local/mariacst/exoplanets/results/posterior/velocity/v100/fixedT10/"
-filepath    = (_path + "N%isigma%.1fb/posterior_" %(nBDs, sigma) + ex)       
+_path = "/home/sven/repos/exoplanets/"
+#filepath    = (_path + "N%isigma%.1fb/posterior_" %(nBDs, sigma) + ex)
+filepath = (_path + "/posterior_" + ex)
 file_object2 = open(filepath + ("_N%i_sigma%.1f_f%.1fgamma%.1frs%.1f"
-                    %(nBDs, sigma, f_true, gamma_true, rs_true))            
-                    + "v" + str(rank), "wb")                                  
+                    %(nBDs, sigma, f_true, gamma_true, rs_true))
+                    + "v" + str(rank), "wb")
 pickle.dump(sampler.flatchain, file_object2, protocol=2)
 file_object2.close()
+
--- running/utils.py
+++ running/utils.py
@@ -5,13 +5,13 @@ from scipy.interpolate import interp1d
 import astropy.units as u
 
 
-# Constant parameters & conversions ==========================================  
-_sigma_sb = sigma_sb.value                                                      
-_G        = G.value                                                             
-conversion_into_K_vs_kg = 1.60217e-7                                            
-conversion_into_w       = 0.16021766                                            
-conv_Msun_to_kg         = 1.98841e+30 # [kg/Msun]                               
-# ============================================================================ 
+# Constant parameters & conversions ==========================================
+_sigma_sb = sigma_sb.value
+_G        = G.value
+conversion_into_K_vs_kg = 1.60217e-7
+conversion_into_w       = 0.16021766
+conv_Msun_to_kg         = 1.98841e+30 # [kg/Msun]
+# ============================================================================
 
 def vc(Rsun, Rint, parameters):
     data = np.genfromtxt("../data/rc_e2bulge_R08.178_J_corr.dat", unpack=True)
@@ -31,14 +31,14 @@ def vgNFW(Rsun, R, parameters):
     # gNFW parameters
     gamma = parameters[0]
     Rs    = parameters[1]
-    rho0  = parameters[2] 
-    v     = []; 
+    rho0  = parameters[2]
+    v     = [];
     for Rint in R:
         hyp=np.float(hyp2f1(3-gamma,3-gamma,4-gamma,-Rint/Rs))
         Integral=(-2**(2+3*gamma)*np.pi*Rint**(3-gamma)*(1+
                   Rsun*(1./Rs))**(3-gamma)*rho0*hyp)/(-3+gamma)
         v.append(np.sqrt(1.18997*10.**(-31.)*Integral/Rint)*3.08567758*10.**(16.))
-    v = np.array(v,dtype=np.float64)      
+    v = np.array(v,dtype=np.float64)
     # Return
     return v
 
@@ -48,20 +48,20 @@ def gNFW_rho(Rsun, R, parameters):
     Denstiy has same units as local DM density rho0
     """
     # gNFW parameters
-    gamma = parameters[0] 
+    gamma = parameters[0]
     Rs    = parameters[1]
     rho0  = parameters[2]
     # Density profile
-    rho   = rho0*np.power(Rsun/R, gamma)*np.power((Rs+Rsun)/(Rs+R), 3-gamma)    
+    rho   = rho0*np.power(Rsun/R, gamma)*np.power((Rs+Rsun)/(Rs+R), 3-gamma)
     # Return
     return rho
 
-def heat_DM(r, f=1, R=R_jup.value, M=M_jup.value, Rsun=8.178, 
+def heat_DM(r, f=1, R=R_jup.value, M=M_jup.value, Rsun=8.178,
             parameters=[1., 20., 0.42], v=None):
     """
     Heat flow due to DM capture and annihilation
     """
-    vesc   = (np.sqrt(2*_G*M/R))*1e-3 # km/s 
+    vesc   = (np.sqrt(2*_G*M/R))*1e-3 # km/s
     if v:
         _vD = v
         #print(_vD, "here i am")
@@ -75,31 +75,31 @@ def heat_DM(r, f=1, R=R_jup.value, M=M_jup.value, Rsun=8.178,
     return (f*np.pi*R**2*_rhoDM*_vDM*(1+3./2.*np.power(vesc/_vD, 2))*
             conversion_into_w) # W
 
-def T_DM(r, R=R_jup.value, M=M_jup.value, Rsun=8.178, f=1., 
-         params=[1., 20., 0.42], v=None, epsilon=1.):                                       
-    """                                                                        
-    DM temperature                                                             
-    """   
+def T_DM(r, R=R_jup.value, M=M_jup.value, Rsun=8.178, f=1.,
+         params=[1., 20., 0.42], v=None, epsilon=1.):
+    """
+    DM temperature
+    """
     # escape velocity
-    vesc   = np.sqrt(2*_G*M/R)*1e-3 # km/s                      
-    if v:                                                                      
-        _vD = v                                                                
-    else:                                                                      
-        _vD    = np.sqrt(3/2.)*vc(Rsun, r, params) # km/s                      
-                                                                               
-    _vDM   =  np.sqrt(8./(3*np.pi))*_vD # km/s                                 
-    _rhoDM = gNFW_rho(Rsun, r, params) # GeV/cm3                               
-    # return                                                                   
-    return np.power((f*_rhoDM*_vDM*(1+3./2.*np.power(vesc/_vD, 2))*    
+    vesc   = np.sqrt(2*_G*M/R)*1e-3 # km/s
+    if v:
+        _vD = v
+    else:
+        _vD    = np.sqrt(3/2.)*vc(Rsun, r, params) # km/s
+
+    _vDM   =  np.sqrt(8./(3*np.pi))*_vD # km/s
+    _rhoDM = gNFW_rho(Rsun, r, params) # GeV/cm3
+    # return
+    return np.power((f*_rhoDM*_vDM*(1+3./2.*np.power(vesc/_vD, 2))*
                     conversion_into_w)/(4*_sigma_sb*epsilon), 1./4.)
 
-def temperature_withDM(r, Tint, R=R_jup.value, M=M_jup.value, 
+def temperature_withDM(r, Tint, R=R_jup.value, M=M_jup.value,
                        f=1., p=[1., 20., 0.42], v=None, Rsun=8.178, epsilon=1):
     """
     Exoplanet temperature : internal heating + DM heating
     """
-    return (np.power(np.power(Tint, 4) + 
-                     np.power(T_DM(r, R=R, M=M, Rsun=Rsun, f=f, params=p, v=v, 
+    return (np.power(np.power(Tint, 4) +
+                     np.power(T_DM(r, R=R, M=M, Rsun=Rsun, f=f, params=p, v=v,
                                    epsilon=epsilon), 4)
                      , 0.25))
 
