8418a1798862dadff14e7061818ae88f402fd4ec
mariabenitocst <mariabenitocst@gmail.com>
2022-01-06 14:25:39 +0200

    updated

--- python/fitting_Ntimes.py
+++ python/fitting_Ntimes.py
@@ -1,93 +1,88 @@
 import sys
-sys.path.append("/home/mariacst/cluster/.env/lib/python3.6/site-packages")
+sys.path.append("/home/mariacst/exoplanets/running/.env/lib/python3.6/site-packages")
 import emcee
 import numpy as np
 from scipy.interpolate import griddata
-from utils import heat, temperature_withDM
-from mock_generation import mock_population
-from astropy.constants import R_jup, M_sun
+import imp
+import mock_generation
+imp.reload(mock_generation)
+from mock_generation import mock_population_all
+from astropy.constants import R_jup
 import glob
 import pickle
-import time
+from scipy.interpolate import interp1d
+from emcee_functions import lnprob
 
-start = time.time()
+#start = time.time()
 
-# Local DM density
-rho0 = 0.42 # GeV/cm3
-
-rank          = 1.
-nBDs          = int(sys.argv[1])
-rel_unc_Tobs  = 0.1
-rel_mass      = 0.1
-f_true        = 1.
-gamma_true    = float(sys.argv[2])
-rs_true       = float(sys.argv[3])
-
-# --------- MCMC fitting -> change to another file -----------------------
-def lnprior(p):
-    f, gamma, rs = p
-    if ( 0. < gamma < 3. and 0. < f < 1. and 0.01 < rs < 50.):
-        return 0.
-    return -np.inf
-
-def residual(p):#, robs, Tobs, rel_unc_Tobs, heat_int, mass):
-    f, gamma, rs = p
-    Tmodel = temperature_withDM(robs, heat_int, f=f, M=mass*M_sun.value, 
-                                parameters=[gamma, rs, rho0])
-    return -0.5*np.sum(((Tmodel-Tobs)/(rel_unc_Tobs*Tobs))**2.)
-
-
-def lnprob(p):#, robs, Tobs, rel_unc_Tobs, heat_int, mass):
-    lp = lnprior(p)
-    if not np.isfinite(lp):
-        # Return
-        return -np.inf
-    # Return
-    return lp + residual(p)#, robs, Tobs, rel_unc_Tobs, heat_int, mass)
+# Constant parameters & conversions ==========================================
+rho0                    = 0.42 # Local DM density [GeV/cm3]
+epsilon                 = 1.
+Rsun                    = 8.178 # Sun galactocentric distance [kpc]
+# ============================================================================
+# Input parameters
+ex         = sys.argv[1]
+rank       = int(sys.argv[2])
+nBDs       = int(sys.argv[3])
+relTobs    = 0.1
+sigma      = float(sys.argv[4])
+f_true     = 1.
+gamma_true = float(sys.argv[5])
+rs_true    = float(sys.argv[6])
+v          = 100.
+Tcut       = 650.
 # ------------------------------------------------------------------------
-
 # Load theoretical cooling model
-path = "./data/"
+path = "/home/mariacst/exoplanets/running/data/"
 data = np.genfromtxt(path + "./ATMO_CEQ_vega_MIRI.txt", unpack=True)
 points = np.transpose(data[0:2, :])
 values = data[2]
-
+# Mock observation
 np.random.seed(42)
-robs, Tobs, mass, ages = mock_population(nBDs, rel_unc_Tobs, rel_mass,
-                                             f_true, gamma_true,
-                                             rs_true, rho0_true=rho0)
-print(Tobs[0], Tobs[467], robs[34])
+(robs, sigmarobs, Tobs, sigmaTobs, Mobs,
+     sigmaMobs, Aobs, sigmaAobs) = mock_population_all(nBDs, relTobs, sigma,
+                                      sigma, sigma, f_true, gamma_true,
+                                      rs_true, rho0_true=rho0, Tmin=Tcut, v=v)
 
-## calculate predictic intrinsic heat flow for mock BDs
-xi = np.transpose(np.asarray([ages, mass]))
+## calculate predictic intrinsic temperature
+xi       = np.transpose(np.asarray([Aobs, Mobs]))
 Teff     = griddata(points, values, xi)
-heat_int = heat(Teff, np.ones(len(Teff))*R_jup.value)
 # ------------------ RECONSTRUCTION --------------------------------------
+# Load variables analytical derivatives Tint
+masses, a, b = np.genfromtxt(path + "derv_ana_wrt_A.dat", unpack=True)
+ages, c = np.genfromtxt(path + "derv_ana_wrt_M.dat", unpack=True)
+a_interp = interp1d(masses, a)
+b_interp = interp1d(masses, b)
+c_interp = interp1d(ages, c)
+
+
+
 ndim     = 3
-nwalkers = 50
+nwalkers = 150
 # first guess
 p0 = [[0.9, 0.9, 20.] + 1e-4*np.random.randn(ndim) for j in range(nwalkers)]
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob) 
-                                #args=(r_obs, Tobs, rel_unc_T, heat_int, mass))
-pos, prob, state  = sampler.run_mcmc(p0, 300, progress=True)
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob,
+              args=(robs, sigmarobs, Mobs, sigmaMobs, Aobs, sigmaAobs, Tobs,
+                    sigmaTobs, Teff, points, values, a_interp, b_interp, 
+                    c_interp, v))
+
+pos, prob, state  = sampler.run_mcmc(p0, 200, progress=False)
 sampler.reset()
-pos, prob, state  = sampler.run_mcmc(pos, 5000, progress=True)
-like    = sampler.flatlnprobability # likelihood
-maxlike = sampler.flatchain[np.argmax(sampler.flatlnprobability)]
-print ("ML estimator : " , maxlike)
+pos, prob, state  = sampler.run_mcmc(pos, 6000, progress=False)
 
 # Save likelihood
-#file_object = open(("likelihood_ex3_N%i_relunc%.2f_relM%.2f_f%.1fgamma%.1frs%.1f" 
-#                    %(nBDs, rel_unc_Tobs, rel_mass, f_true, gamma_true, rs_true))
-#                    + "v" + str(rank), "wb")
-#pickle.dump(like, file_object, protocol=2)
-#file_object.close()
+_path = "/hdfs/local/mariacst/exoplanets/results/likelihood/velocity/v100/analytic/fixedT10Tcut650_nocutTwn/"
+filepath    = (_path + "N%isigma%.1f/like_" %(nBDs, sigma) + ex)
+file_object = open(filepath + ("_N%i_sigma%.1f_f%.1fgamma%.1frs%.1f"
+                    %(nBDs, sigma, f_true, gamma_true, rs_true))            
+                    + "v" + str(rank), "wb") 
+pickle.dump(sampler.flatlnprobability, file_object, protocol=2)
+file_object.close() 
 # Save posterior
-file_object = open(("posterior_ex3_N%i_relunc%.2f_relM%.2f_f%.1fgamma%.1frs%.1f" 
-                    %(nBDs, rel_unc_Tobs, rel_mass, f_true, gamma_true, rs_true))
-                    + "v" + str(rank), "wb")
-pickle.dump(sampler.flatchain, file_object, protocol=2)
-file_object.close()
-
-
-print("Running time in s =", time.time()-start)
+_path = "/hdfs/local/mariacst/exoplanets/results/posterior/velocity/v100/analytic/fixedT10Tcut650_nocutTwn/"
+filepath    = (_path + "N%isigma%.1f/posterior_" %(nBDs, sigma) + ex)       
+file_object2 = open(filepath + ("_N%i_sigma%.1f_f%.1fgamma%.1frs%.1f"
+                    %(nBDs, sigma, f_true, gamma_true, rs_true))            
+                    + "v" + str(rank), "wb")                                  
+pickle.dump(sampler.flatchain, file_object2, protocol=2)
+file_object2.close()
