dd3e6b74865b34a2870e05d22b96fffca8651efe
mariabenitocst <mariabenitocst@gmail.com>
2022-01-10 15:47:11 +0200

    mixed dervs

--- python/experimental_sensitivity_CL_Asimov.py
+++ python/experimental_sensitivity_CL_Asimov.py
@@ -10,6 +10,7 @@ from scipy.interpolate import griddata
 from utils import T_DM, temperature_withDM
 from astropy.constants import R_jup, M_sun
 from scipy.stats import percentileofscore
+from derivatives import derivativeTint_wrt_M, derivativeTint_wrt_A
 from derivatives import derivativeTDM_wrt_M, derivativeTDM_wrt_r, derivativeTintana_wrt_A
 from scipy.interpolate import interp1d
 
@@ -41,6 +42,7 @@ def sigma_Tmodel2(r, M, A, sigma_r, sigma_M, sigma_A,
                                                                                    
     dervT_M = ((Tint/Ttot)**3* c(A) +                                              
                (_TDM/Ttot)**3*derivativeTDM_wrt_M(r, f, params, M, v))             
+    
     # return                                                                    
     return (np.power((Tint/Ttot)**3*derivativeTintana_wrt_A(M, A, a, b)*sigma_A, 2)+ 
             np.power(dervT_M*sigma_M, 2)+                                          
@@ -137,17 +139,94 @@ def p_value_b(gamma_k, f, rs, nBDs, relT, relM, relR, relA, points, values,
     # return
     return 100-percentileofscore(TS_k, TS_obs, kind="strict") 
 
+def sigma_Tmodel2_obs(r, M, A, sigma_r, sigma_M, sigma_A, Tint, dervTint_M, 
+                  dervTint_A, f, params, v=None, R=R_jup.value, Rsun=8.178, 
+                  epsilon=1):               
+    """                                                                        
+    Return squared uncertainty in model temperature [UNITS??]                  
+                                                                               
+    Input:                                                                     
+        r : Galactocentric distance [kpc]                                      
+        M : mass [Msun]                                                        
+        A : age [Gyr]                                                          
+                                                                               
+    Assumption: uncertainties in age, mass and galactocentric distance         
+        are independent                                                        
+    """                                                                        
+    M_in_kg = M*conv_Msun_to_kg                                                
+                                                                               
+    _TDM = T_DM(r, R=R, M=M_in_kg, Rsun=Rsun, f=f, params=params, v=v,         
+                epsilon=epsilon)                                               
+    Ttot = np.power(_TDM**4 + Tint**4, 0.25)                                   
+                                                                               
+    dervT_M = ((Tint/Ttot)**3*dervTint_M +                                     
+               (_TDM/Ttot)**3*derivativeTDM_wrt_M(r, f, params, M, v=v, R=R,   
+                                                  Rsun=Rsun,epsilon=epsilon))  
+    # return                                                                   
+    return (np.power((Tint/Ttot)**3*dervTint_A*sigma_A, 2)+                    
+            np.power(dervT_M*sigma_M, 2)+                                      
+            np.power((_TDM/Ttot)**3*derivativeTDM_wrt_r(r, f, params, M, v=v,  
+                                  R=R, Rsun=Rsun, epsilon=epsilon)*sigma_r, 2))
+
+def lnL_sb_obs(gamma, f, rs, robs, sigma_robs, Mobs, sigma_Mobs, Aobs, sigma_Aobs,
+           Tobs, sigma_Tobs, Tint, dervTint_M, dervTint_A,
+           v=None, R=R_jup.value, Rsun=8.178, rho0=0.42, epsilon=1.):
+    """
+    Return ln(L) assuming predicted temperature = DM + intrinsic
+    """  
+    # Calculate predicted temperature (DM + intrinsic)
+    Tmodel = temperature_withDM(robs, Tint, M=Mobs*conv_Msun_to_kg, f=f,
+                           p=[gamma, rs, rho0], v=v)
+    
+    _sigma_Tmodel2 = sigma_Tmodel2_obs(robs, Mobs, Aobs, sigma_robs, sigma_Mobs,   
+                                   sigma_Aobs, Tint, dervTint_M, dervTint_A,   
+                                   f, [gamma, rs, rho0], v=v, R=R, Rsun=Rsun,  
+                                   epsilon=epsilon)                            
+    # return                                                                   
+    return -0.5*np.sum(np.log(sigma_Tobs**2 + _sigma_Tmodel2) + 
+                       (Tmodel-Tobs)**2/(sigma_Tobs**2 + _sigma_Tmodel2)) 
+
+
+def lnL_b_obs(sigma_Mobs, sigma_Aobs, Tobs, sigma_Tobs, Tint, dervTint_M, 
+          dervTint_A):
+    """
+    Return ln(L) assuming predicted temperature = intrinsic
+    """  
+    
+    sigma_Tint2 = (np.power(dervTint_A*sigma_Aobs, 2) + 
+                   np.power(dervTint_M*sigma_Mobs, 2))
+    
+    # return
+    return -0.5*np.sum(np.log(sigma_Tobs**2 + sigma_Tint2) + 
+                       (Tint-Tobs)**2/(sigma_Tobs**2 + sigma_Tint2)) 
+
+def _TS_obs(gamma, f, rs, robs, sigma_robs, Mobs, sigma_Mobs, Aobs, sigma_Aobs,  
+       Tobs, sigma_Tobs, Tint, dervTint_M, dervTint_A,                         
+       v=None, R=R_jup.value, Rsun=8.178, rho0=0.42, epsilon=1.):
+    """
+    Test statistics
+    """
+    # return
+    return (-2.*lnL_sb_obs(gamma, f, rs, robs, sigma_robs, Mobs, sigma_Mobs, 
+                       Aobs, sigma_Aobs, Tobs, sigma_Tobs, Tint, dervTint_M, 
+                       dervTint_A, v=v, R=R, Rsun=Rsun, rho0=rho0, 
+                       epsilon=epsilon)
+            +2*lnL_b_obs(sigma_Mobs, sigma_Aobs, Tobs, sigma_Tobs, Tint, 
+                     dervTint_M, dervTint_A)
+            )
+
 
 def UL_at_rs(rs, f, nBDs, relT, relM, relR, relA, points, values,
              robs, sigmarobs, Mobs, sigmaMobs, Aobs, sigmaAobs, Tobs, 
-             sigmaTobs, Teff, a, b, c, 
+             sigmaTobs, Teff, a, b, c, dervTint_A, dervTint_M,
              steps=300, rho0=0.42, v=None, gamma_min=0.01, gamma_max=2.95):
     # Grid in gamma
     gamma_k = np.linspace(gamma_min, gamma_max, 30) # change this?             
     for g in gamma_k:                                                          
         # Observed TS                                                          
-        TS_obs = TS(g, f, rs, robs, sigmarobs, Mobs, sigmaMobs, Aobs,     
-                    sigmaAobs, Tobs, sigmaTobs, Teff, a, b, c, v=v)                                
+        TS_obs = _TS_obs(g, f, rs, robs, sigmarobs, Mobs, sigmaMobs, Aobs,     
+                         sigmaAobs, Tobs, sigmaTobs, Teff, dervTint_M, dervTint_A, 
+                         v=v)                
         # s + b hypothesis                                                     
         _p_sb = p_value_sb(g, f, rs, nBDs, relT, relM, relR, relA,          
                            points, values, a, b, c, TS_obs, steps=steps, v=v)           
@@ -192,7 +271,17 @@ if __name__=="__main__":
                                       sigma, sigma, sigma, 0., 1., 1., 
                                       Tmin=0., v=v)                        
     xi   = np.transpose(np.asarray([Aobs, Mobs]))                               
-    Teff = griddata(points, values, xi)                                         
+    Teff = griddata(points, values, xi) 
+    # Calculate derivatives Tint wrt Age and Mass                               
+    dervTint_A = np.ones(nBDs)                                                  
+    dervTint_M = np.ones(nBDs)                                                  
+    size       = 7000                                                           
+    h          = 0.001                                                          
+    for i in range(nBDs):                                                       
+        dervTint_A[i] = derivativeTint_wrt_A(Mobs[i], Aobs[i], points, values,  
+                                         size=size, h=h)                        
+        dervTint_M[i] = derivativeTint_wrt_M(Mobs[i], Aobs[i], points, values,  
+                                         size=size, h=h)                                          
 
     # Load variables analytical derivatives Tint                                       
     path = "/home/mariacst/exoplanets/running/data/"
@@ -208,7 +297,7 @@ if __name__=="__main__":
         gamma_up[i] = UL_at_rs(_rs, f, nBDs, relT, sigma, sigma, sigma, points, 
                                values, robs, sigmarobs, Mobs, sigmaMobs, Aobs,
                                sigmaAobs, Tobs, sigmaTobs, Teff, a_interp, b_interp, c_interp, 
-                               steps=steps, v=v, 
+                               dervTint_A, dervTint_M, steps=steps, v=v, 
                                gamma_min=gamma_min[i], gamma_max=gamma_max[i])
         print("%.1f  %.4f" %(_rs, gamma_up[i]))
         i+=1
